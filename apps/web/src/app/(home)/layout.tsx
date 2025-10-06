905226d734eda3b1550f01c011a6e6357d8188ee
brick.ninja <dev@brick.ninja>
2025-10-06 22:43:04 +0200

    feat(layout): add footer with documentation and legal links

--- apps/web/src/app/(home)/layout.tsx
+++ apps/web/src/app/(home)/layout.tsx
@@ -1,9 +1,13 @@
+import type { Route } from 'next';
+
+import NextLink from 'next/link';
 import { HomeLayout } from 'fumadocs-ui/layouts/home';
 
 import { baseOptions } from '@/app/layout.config';
 import { LinkButton } from '@/components/button';
 import { Iconify } from '@/components/iconify/iconify.client';
 import { getUser } from '@/lib/session';
+import { Link } from '@heroui/react';
 
 export default async function Layout({ children }: LayoutProps<'/'>) {
   const user = await getUser();
@@ -46,6 +50,14 @@ export default async function Layout({ children }: LayoutProps<'/'>) {
     >
       <div aria-hidden="true" className="gradient-background home-gradient-background"/>
       {children}
+      <footer className="flex flex-wrap justify-between gap-4 py-4 px-8 border-t">
+        <div className="text-inherit"><b>bn2.me</b> by <Link href="https://brick.ninja">brick.ninja</Link> © {new Date().getFullYear()}</div>
+        <div className="flex flex-wrap gap-4">
+          <NextLink href="/dev/docs">Developer Documentation</NextLink>
+          <NextLink href={'/legal' as Route}>Legal Notice</NextLink>
+          <NextLink href="/legal/privacy-policy">Privacy Policy</NextLink>
+        </div>
+      </footer>
     </HomeLayout>
   );
 }
